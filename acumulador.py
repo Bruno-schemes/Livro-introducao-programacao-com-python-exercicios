668aeaad42ec2f38b5e33a8215856c67ce53fc4e
Bruno-schemes <73672114+Bruno-schemes@users.noreply.github.com>
2022-07-13 02:51:06 -0300

    Add files via upload
    exercício com acumulador, com comentários para facilitar no entendimento

---
+++ acumulador.py
@@ -0,0 +1,21 @@
+# n = 1
+# soma = 0
+# while n <= 10:
+#     x = int(input(f"digite o {n} numero: ")) # enquanto n(1) for menor ou igual a 10, 
+#     soma = soma + x #soma o "valor digitado" + "soma"(0)
+#     n = n + 1 # acumulado de numero // ou seja o primeiro print começara do "1",EX: "digite o 1 numero: "
+#     print(f"Soma: {soma}") #aqui ele printa a nova "soma" 
+    
+
+
+
+x = 1 
+soma = 0
+while x <= 5:
+    n = int(input(f"{x} Digite o número: ")) # valor digitado
+    soma = soma + n # somando 0 + valor digitado
+    x = x + 1 # acumulador de numero
+    print(f"Média: {soma / 5:5.2f}") # novo valor adquirido da soma + valor digitado, dividido por 5   
+        # consideração final para o eu do futuro kkkk.... 
+
+        #resultado do calculo indica quantas vezes  é possivel dividir o numero digitado
