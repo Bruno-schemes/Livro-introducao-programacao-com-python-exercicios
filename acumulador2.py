aeec7e889b243a903b32747d6bc97fe6d89f96d4
Bruno-schemes <73672114+Bruno-schemes@users.noreply.github.com>
2022-07-15 01:49:04 -0300

    Add files via upload
    explicando um pouco mais sobre acumulador

---
+++ acumulador2.py
@@ -0,0 +1,7 @@
+tabuada = int(input("Digite o valor da tabuada: "))
+acumulador = 1
+
+while acumulador <= 10:
+    resultado = tabuada * acumulador
+    print(resultado)
+    acumulador = acumulador + 1
